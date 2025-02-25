ad3614b9037a22d0deee9fd96f7efd4e9503c7b7
Daniel Fly <danielfly42@gmail.com>
2025-02-26 00:43:46 +0100

    Added player API controller and Create endpoint

--- src/BowlingTrackerSupreme.Domain/Models/Player.cs
+++ src/BowlingTrackerSupreme.Domain/Models/Player.cs
@@ -1,11 +1,14 @@
-﻿namespace BowlingTrackerSupreme.Domain.Models;
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace BowlingTrackerSupreme.Domain.Models;
 
 public class Player
 {
-    public Guid Id { get; set; }
+    public Guid? Id { get; set; }
     public required string Name { get; set; }
-    public DateTime CreatedOn { get; set; }
-    public DateTime ModifiedOn { get; set; }
+    public DateTime? CreatedOn { get; set; }
+    public DateTime? ModifiedOn { get; set; }
 
     public IEnumerable<Game> GameParticipation { get; set; } = [];
     public IEnumerable<PlayerGame> PlayedGames { get; set; } = [];
